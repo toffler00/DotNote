d75758faafbe074d81259a04d7a9710dedf0fc5b
ssy828 <828yss@gmail.com>
2018-09-05 15:29:40 +0900

    modified Auto Layout

--- Orbit/ListTableViewCell.swift
+++ Orbit/ListTableViewCell.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ListTableViewCell: UITableViewCell {
     
+    // MARK: propeties
+    internal var dateLabel: UILabel = UILabel()
+    internal var titleLabel: UILabel = UILabel()
+    internal var weekLabel: UILabel = UILabel()
     var model: Model.Contents! {
         didSet {
-            //ToDo //////
+            //ToDo
             self.titleLabel.text = model.title
         }
     }
     
-    internal var dateLabel: UILabel = UILabel()
-    internal var titleLabel: UILabel = UILabel()
-    internal var weekLabel: UILabel = UILabel()
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -35,6 +36,11 @@ class ListTableViewCell: UITableViewCell {
         // ToDo
     }
     
+    // MARK: method
+    func didChangeString(forDate date: Date) {
+        let dateFomatter = DateFormatter()
+        dateFomatter.dateFormat = ""
+    }
 }
 // MARK: - extension ListTableViewCell
 extension ListTableViewCell {
@@ -46,6 +52,9 @@ extension ListTableViewCell {
         self.titleLabel.textAlignment = .center
         self.weekLabel.textAlignment = .center
         
+        // numberOfLines
+        self.titleLabel.numberOfLines = 0
+        
         // translatesAutoresizingMaskIntoConstraints
         self.dateLabel.translatesAutoresizingMaskIntoConstraints = false
         self.titleLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -53,20 +62,20 @@ extension ListTableViewCell {
         
         // NSLayoutConstraint
         // title label Constraints
-        let titleLabelConstraints: [NSLayoutConstraint] = [NSLayoutConstraint(item: self.titleLabel, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1, constant: 0),NSLayoutConstraint(item: self.titleLabel, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0),NSLayoutConstraint(item: self.titleLabel, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 50),NSLayoutConstraint(item: self.titleLabel, attribute: .width, relatedBy: .equal, toItem: self, attribute: .width, multiplier: 0.5, constant: 0)]
+        let titleLabelConstraints: [NSLayoutConstraint] = [NSLayoutConstraint(item: self.titleLabel, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1, constant: 0),NSLayoutConstraint(item: self.titleLabel, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0),NSLayoutConstraint(item: self.titleLabel, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 50),NSLayoutConstraint(item: self.titleLabel, attribute: .width, relatedBy: .equal, toItem: self, attribute: .width, multiplier: 0.55, constant: 0)]
         // 중앙 먼저
         self.addSubview(titleLabel)
         self.addConstraints(titleLabelConstraints)
         
         // date Label Constraints
-        let dateLabelConstraints: [NSLayoutConstraint] = [NSLayoutConstraint(item: self.dateLabel, attribute: .centerX, relatedBy: .equal, toItem: self.titleLabel, attribute: .centerX, multiplier: 0.5, constant: 0),NSLayoutConstraint(item: self.dateLabel, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0),NSLayoutConstraint(item: self.dateLabel, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 50),NSLayoutConstraint(item: self.dateLabel, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1, constant: 10),NSLayoutConstraint(item: self.dateLabel, attribute: .trailing, relatedBy: .equal, toItem: self.titleLabel, attribute: .leading, multiplier: 1, constant: 0)]
+        let dateLabelConstraints: [NSLayoutConstraint] = [NSLayoutConstraint(item: self.dateLabel, attribute: .centerX, relatedBy: .equal, toItem: self.titleLabel, attribute: .centerX, multiplier: 0.5, constant: 0),NSLayoutConstraint(item: self.dateLabel, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0),NSLayoutConstraint(item: self.dateLabel, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 50),NSLayoutConstraint(item: self.dateLabel, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1, constant: 5),NSLayoutConstraint(item: self.dateLabel, attribute: .trailing, relatedBy: .equal, toItem: self.titleLabel, attribute: .leading, multiplier: 1, constant: 0),NSLayoutConstraint(item: self.dateLabel, attribute: .width, relatedBy: .equal, toItem: self, attribute: .width, multiplier: 0.25, constant: 0)]
         
 
         self.addSubview(dateLabel)
         self.addConstraints(dateLabelConstraints)
 
         // week label Constraints
-        let weekLabelConstraints: [NSLayoutConstraint] = [NSLayoutConstraint(item: self.weekLabel, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1.5, constant: 0),NSLayoutConstraint(item: self.weekLabel, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0),NSLayoutConstraint(item: self.weekLabel, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 50),NSLayoutConstraint(item: self.weekLabel, attribute: .leading, relatedBy: .equal, toItem: self.titleLabel, attribute: .trailing, multiplier: 1, constant: 0),NSLayoutConstraint(item: self.weekLabel, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1, constant: 10)]
+        let weekLabelConstraints: [NSLayoutConstraint] = [NSLayoutConstraint(item: self.weekLabel, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1.5, constant: 0),NSLayoutConstraint(item: self.weekLabel, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0),NSLayoutConstraint(item: self.weekLabel, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 50),NSLayoutConstraint(item: self.weekLabel, attribute: .leading, relatedBy: .equal, toItem: self.titleLabel, attribute: .trailing, multiplier: 1, constant: 0),NSLayoutConstraint(item: self.weekLabel, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1, constant: 5),NSLayoutConstraint(item: self.weekLabel, attribute: .width, relatedBy: .equal, toItem: self, attribute: .width, multiplier: 0.25, constant: 0)]
         
 
         self.addSubview(weekLabel)
